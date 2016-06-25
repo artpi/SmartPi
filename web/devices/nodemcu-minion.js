8594d0073d11e4bdbc2146b786e545406fa8714f
artpi <artpi@post.pl>
2016-06-25 22:04:12 +0200

    Few UI improvements

--- web/devices/nodemcu-minion.js
+++ web/devices/nodemcu-minion.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import pick from 'lodash/pick';
+import some from 'lodash/some';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
-import Toggle from 'material-ui/Toggle';
+import Chip from 'material-ui/Chip';
 import RGBControl from '../modes/rgb-control.js';
 import Switch from '../modes/switch.js';
+import { red500, cyan200 } from 'material-ui/styles/colors';
 
 class NodemcuMinion extends Component {
 	constructor( props, context ) {
@@ -32,19 +34,16 @@ class NodemcuMinion extends Component {
 		return (
 			<Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange} style={ { margin: 10 } }>
 				<CardHeader
-					title={ this.props.name || this.props.id }
-					subtitle={ this.props.mode + " Minion" }
 					actAsExpander={ true }
 					showExpandableButton={ true }
 				>
-					<Toggle
-						toggled={ this.props.online }
-						disabled={ true }
-						labelPosition="right"
-						label={ this.props.online ? "ONLINE" : "OFFLINE" }
-					/>
+				<div style={ { display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '20px', marginRight: '50px' } }>
+					<b>{ this.props.name || this.props.id }</b>
+					{ some( Object.keys( this.props.state ), key => !! this.props.state[ key ] ) ? <Chip backgroundColor={ cyan200 } onRequestDelete={ this.off.bind( this ) }>ON</Chip> : null }
+					{ ( ! this.props.online ) ? <Chip backgroundColor={ red500 } labelColor={ 'white' }>OFFLINE</Chip> : null }
+				</div>
 				</CardHeader>
-				<CardText expandable={true}>
+				<CardText expandable={ true }>
 				{
 					this.props.mode === 'switch' ? <Switch
 						dispatch={ this.dispatch.bind( this ) }
