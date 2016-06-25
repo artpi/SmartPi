d02396a87dac196cd7e7127da3dfb3fa3c2f2cb2
madeincosmos <maria@madeincosmos.net>
2016-06-25 13:07:30 +0200

    Color picker
    * Added color picker for RGB strip
    
    * Added gradient background for sliders in colorpicker
    
    * Color picker fixes

---
+++ web/components/colorPicker.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { CustomPicker } from 'react-color';
+import Slider from 'material-ui/Slider';
+
+class ColorPicker extends React.Component {
+    render() {
+        var styles = {
+            example: {
+                width: '100%',
+                height: '50px',
+                border: '2px solid black',
+                marginBottom: '2em',
+                backgroundColor: this.props.hex
+            },
+            hue: {
+                background: 'linear-gradient(to right, #f00 0%, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%)',
+                height: '10px'
+            },
+            saturation: {
+                background: 'linear-gradient(to right, #888 0%, hsl(' + this.props.hsl.h + ', 100%, 50%) 100%)',
+                height: '10px'
+            },
+            lightness: {
+                background: 'linear-gradient(to right, #000 0%, hsl(' + this.props.hsl.h + ', 100%, 50%) 50%, #fff 100%)',
+                height: '10px'
+            },                        
+            
+        }
+
+        return (
+            <div>
+                <div style={styles.example}></div>
+                <Slider 
+                    name="h"
+                    min={0}
+                    max={360}
+                    step={1}
+                    style={styles.hue}
+                    value={this.props.hsl.h}
+                    onChange={ (e, val) => this.props.onChange({
+                        h: val,
+                        s: 1,
+                        l: this.props.hsl.l
+                    })}
+                />
+                <div>Hue</div>                             
+
+                <Slider 
+                    name="l"
+                    min={0}
+                    max={1}
+                    step={.001}
+                    style={styles.lightness}                    
+                    value={this.props.hsl.l}
+                    onChange={ (e, val) => this.props.onChange({
+                        h: this.props.hsl.h,
+                        s: 1,
+                        l: val
+                    })}
+                />
+                <div>Lightness</div>                
+            </div>
+        );       
+    }
+}
+
+export default CustomPicker(ColorPicker);
--- web/devices/nodemcu-minion.js
+++ web/devices/nodemcu-minion.js
@@ -5,20 +5,29 @@ import pick from 'lodash/pick';
 import TextField from 'material-ui/TextField';
 import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card';
 import Toggle from 'material-ui/Toggle';
+import ColorPicker from '../components/colorPicker';
 
 class NodemcuMinion extends Component {
 	constructor( props, context ) {
 		super( props, context );
 		this.state = {
-			open: false,
+			open: false
 		};
 	}
 
-	submit() {
+	submit( color ) {
+        var red = Math.round(color.rgb.r * 4); 
+        var green = Math.round(color.rgb.g * 4); 
+        var blue = Math.round(color.rgb.b * 4);
+        
 		this.props.dispatch( {
 			id: this.props.id,
 			action: 'set',
-			state: pick( this.state, [ 'red', 'green', 'blue' ] )
+			state: {
+                red: red,
+                green: green,
+                blue: blue
+            }
 		} );
 	}
 
@@ -30,6 +39,17 @@ class NodemcuMinion extends Component {
 	}
 
 	render() {
+
+        var red = get( this.props, [ 'state', 'red' ], '' );
+        var green = get( this.props, [ 'state', 'green' ], '' );
+        var blue = get( this.props, [ 'state', 'blue' ], '' );
+        
+        var initialColor = {
+            r: Math.round(red / 4),
+            g: Math.round(green / 4),
+            b: Math.round(blue / 4)
+        }            
+
 		return (
 			<Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange} style={ { margin: 10 } }>
 				<CardHeader
@@ -48,17 +68,10 @@ class NodemcuMinion extends Component {
 				<CardTitle title="RGB strip" subtitle="Control RGB settings here" expandable={true} />
 
 				<CardText expandable={ true }>
-					<div>
-						<TextField floatingLabelText="RED" type={ 'number' } value={ this.state.red || get( this.props, [ 'state', 'red' ], '' ) } onChange = { ( e, val ) => this.setState( { 'red' : val } ) }/>
-						<br />
-						<TextField floatingLabelText="GREEN" type={ 'number' } value={ this.state.green || get( this.props, [ 'state', 'green' ], '' ) } onChange = { ( e, val ) => this.setState( { 'green' : val } ) }/>
-						<br />
-						<TextField floatingLabelText="BLUE" type={ 'number' } value={ this.state.blue || get( this.props, [ 'state', 'blue' ], '' ) } onChange = { ( e, val ) => this.setState( { 'blue' : val } ) }/>
-						<br />
-					</div>
+                    <ColorPicker color={ initialColor }  onChangeComplete={ this.submit.bind( this ) } />
+
 				</CardText>
 				<CardActions expandable={ true }>
-					<FlatButton label="SAVE" onClick={ () => { this.submit() } } />
 					<FlatButton label="OFF" onClick={ () => { this.off() } } />
 				</CardActions>
 			</Card>
