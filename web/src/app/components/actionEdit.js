1e0c9f69a8ea7f03ad79a4c854224f776f547611
artpi <artpi@post.pl>
2016-06-27 12:55:15 +0200

    Esformatter

--- web/src/app/components/actionEdit.js
+++ web/src/app/components/actionEdit.js
@@ -9,29 +9,27 @@ import ActionDelete from 'material-ui/svg-icons/action/highlight-off';
 class ActionEdit extends Component {
 	render() {
 		var deleteIcon = {
-			position: 'absolute',
-			right: 0,
-			bottom: 18
-		},
-		title = this.props.action;
+				position: 'absolute',
+				right: 0,
+				bottom: 18
+			},
+			title = this.props.action;
 
 		if ( this.props.duration ) {
 			title += ' for ' + this.props.duration / 1000 + ' seconds';
 		}
 		title += ' on ' + this.props.id;
 
-		
-		return ( 
-			<Card key={ this.props.index } style={ { marginTop: '20px' } } >
+		return (
+			<Card key={ this.props.index } style={ { marginTop: '20px' }} >
 				<CardHeader
 					title={ title }
 					actAsExpander={ true }
 					showExpandableButton={ true }
 					avatar={
-						<Avatar backgroundColor={ this.props.color }>{ this.props.index }</Avatar>
-						}
-				>
-				</CardHeader>
+					<Avatar backgroundColor={ this.props.color }>{ this.props.index }</Avatar>
+					}
+				/ >
 				<CardText expandable={ true }>
 					{ 	this.props.duration ?
 						<TextField
@@ -40,19 +38,18 @@ class ActionEdit extends Component {
 							floatingLabelFixed={true}
 							onChange={ ( e, val ) => {
 								if ( val && val !== '' ) {
-									this.props.dispatch( { duration: val } )
+									this.props.dispatch( { duration: val } );
 								}
-							}  }
+							} }
 						/> : <span></span>
 					}
-					{ /* Przychaczone */ this.props.color ? <DeviceMode mode="rgb" fetching={ false } dispatch={ (color) => this.props.dispatch( { state: color } ) } state={ this.props.state } /> : <b>Sorry, not ready yet</b> }
-					
+					{ /* Przychaczone */ this.props.color ? <DeviceMode mode="rgb" fetching={ false } dispatch={ ( color ) => this.props.dispatch( { state: color } ) } state={ this.props.state } /> : <b>Sorry, not ready yet</b> }
 				</CardText>
 				<CardActions expandable={ true }>
 					<FlatButton style={ deleteIcon } label='Delete' onTouchTap={ this.props.delete } icon={ <ActionDelete /> } />				
 				</CardActions>
-			</Card> 
-		);
+			</Card>
+			);
 	}
 }
 
